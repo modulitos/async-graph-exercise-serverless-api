487c83df40cbdb916c304d133d8e614ac5a8f5c2
modulitos <modulitos@users.noreply.github.com>
2020-08-20 15:54:08 -0700

    Added graph as a static lifetime, but it cannot read the file?
    Here is the full response in AWS:
    Response:
    {
      "errorMessage": "RequestId: 6a1f9069-8255-4e04-b5f8-8551e8294780 Error: Runtime exited with error: exit status 101",
      "errorType": "Runtime.ExitError"
    }
    
    Request ID:
    "6a1f9069-8255-4e04-b5f8-8551e8294780"
    
    Function logs:
    START RequestId: 6a1f9069-8255-4e04-b5f8-8551e8294780 Version: $LATEST
    thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Os { code: 2, kind: NotFound, message: "No such file or directory" }', src/main.rs:52:18
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    END RequestId: 6a1f9069-8255-4e04-b5f8-8551e8294780
    REPORT RequestId: 6a1f9069-8255-4e04-b5f8-8551e8294780    Duration: 43.53 ms    Billed Duration: 100 ms    Memory Size: 128 MB    Max Memory Used: 5 MB
    RequestId: 6a1f9069-8255-4e04-b5f8-8551e8294780 Error: Runtime exited with error: exit status 101
    Runtime.ExitError

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,51 @@ use std::error::Error;
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
     #[serde(rename = "firstName")]
-    first_name: String,
+    first_name: NodeId,
 }
 
-#[derive(Serialize, Clone)]
-struct CustomOutput {
-    message: String,
+// Inspired by this implementation: https://github.com/awslabs/aws-lambda-rust-runtime/issues/123
+
+static INSTANCE: OnceCell<Graph> = OnceCell::new();
+
+#[derive(Serialize)]
+struct ApiNode {
+    // TODO: consider mapping this into a string of the full URL
+    children: Vec<NodeId>,
+    reward: u32,
 }
+
+impl From<&Node> for ApiNode {
+    fn from(node: &Node) -> Self {
+        ApiNode {
+            reward: node.reward,
+            children: node.children.clone(),
+        }
+    }
+}
+
 
 // The entry point of our bootstrap executable. This is the code that will run when Lambda starts
 // our function:
 
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Info)?;
+    // ERROR: we're getting the following error in AWS Lambda:
+    //
+    // Os { code: 2, kind: NotFound, message: "No such file or directory" }
+    INSTANCE.set(Graph::new().unwrap()).ok();
     lambda!(my_handler);
 
     Ok(())
 }
 
-fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        error!("Empty first name in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty first name"));
-    }
+fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<ApiNode, HandlerError> {
+    let graph = INSTANCE.get().expect("cannot get graph");
 
-    Ok(CustomOutput {
-        message: format!("Hello2, {}!", e.first_name),
-    })
+    if let Some(node) = graph.get(e.first_name) {
+        Ok(ApiNode::from(node))
+    } else {
+        error!("No node found in request {}", c.aws_request_id);
+        Err(c.new_error("cannot find the node"))
+    }
 }
